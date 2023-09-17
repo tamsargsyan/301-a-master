67d0fb2e2506957a7ab22027661e468449235023
Tam Sargsyan <62563325+tamsargsyan@users.noreply.github.com>
2023-09-17 17:26:45 +0400

    faq api integration, fix faq scrolling bug

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -11,51 +11,11 @@ import PATTERN_2 from "../../assets/patterns/about-us/big-pattern-2.svg";
 import PATTERN_3 from "../../assets/patterns/about-us/big-pattern-3.svg";
 import { fetchingAboutUs } from "../../actions/apiActions";
 import { Spin, Collapse } from "antd";
-import type { CollapseProps } from "antd";
 import "./index.css";
 import Img from "../../components/Img";
+import { removeHtmlTags } from "../../globalFunctions/removeHtmlTags";
 
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
-
-const ontologyItems: CollapseProps["items"] = [
-  {
-    key: "1",
-    label: "Каково место человека в мире ?",
-    children: <p>{text}</p>,
-  },
-  {
-    key: "2",
-    label: "Что такое онтологическ ий захват ?",
-    children: <p>{text}</p>,
-  },
-  {
-    key: "3",
-    label: "Что такое онтологическ ая безопасность ?",
-    children: <p>{text}</p>,
-  },
-];
-
-const questionsItems: CollapseProps["items"] = [
-  {
-    key: "1",
-    label: "Что такое армянское кино?",
-    children: <p>{text}</p>,
-  },
-  {
-    key: "2",
-    label: "Что такое армянское образование?",
-    children: <p>{text}</p>,
-  },
-  {
-    key: "3",
-    label: "Что такое армянская культура?",
-    children: <p>{text}</p>,
-  },
-];
+const { Panel } = Collapse;
 
 const AboutUs = () => {
   const data1 = [
@@ -77,12 +37,13 @@ const AboutUs = () => {
 
   useEffect(() => {
     if (
+      data &&
       window.location.hash === "#what-are-we-doing" &&
       whatAreWeDoingRef.current
     ) {
       whatAreWeDoingRef.current.scrollIntoView();
     }
-  }, [whatAreWeDoingRef]);
+  }, [whatAreWeDoingRef, data]);
 
   if (loading)
     return (
@@ -92,24 +53,7 @@ const AboutUs = () => {
     );
 
   const infos = Object.values(data);
-  const faq = {
-    title_am: "",
-    title_ru: "Онтологическая безопасность — F.A.Q.",
-    title_en: "",
-    short_description_am: "",
-    short_description_ru: "<p>Что такое онтология?</p>",
-    short_description_en: "",
-    description: [
-      "<p><span class='mentioned_link'>Онтология</span> — один из крупнейших разделов философии. Онатология изучает основные принципы бытия: его начало, свойства и формы. Слово «онтология» пришло к нам из греческого языка (онтос – сущее, логос – учение) и означает учение о сущем, сущностном, самом важном.</p>",
-      "<p><span class='mentioned_link'>Онтология</span> — это система мировидения, мышление о мире. К примеру, когда-то человечество существовало в онтологии Плоской Земли — это накладывало отпечаток на все: науку, технологию, культуру.</p>",
-      "<p><span class='mentioned_link'>Онтология</span> — база мышления, система видения мира, на которой основывается все остальное. К тому же онтология — это культурологический клей, он соединяет нацию, культуру, систему ценностей, территориальную айдентику.</p>",
-      "<p>К примеру, технологически мы можем приехать в Тибет на электричке, однако — это обесценивает большую часть ритуалов, связанных с походом в Лхасу. В данном случае, легкая достижимость ведет к кризису культуры — культура схлопывается и становится внешней.</p>",
-      "<p><span class='mentioned_link'>Потеря онтологии народа — это потеря ключа к его культуре.</span></p>",
-    ],
-  };
-  const onChange = (key: string | string[]) => {
-    console.log(key);
-  };
+  const faq = infos[5];
 
   return (
     <>
@@ -139,49 +83,52 @@ const AboutUs = () => {
             )
           );
         })}
-        <div>
-          <Header
-            title={faq.title_ru}
-            shortDescription={faq.short_description_ru}
-            description={""}
-            faqDesc={faq.description}
-            id='#what-are-we-doing'
-          />
-          <p className='faq_header'>На какие вопросы отвечает Онтология?</p>
-          <Collapse
-            items={ontologyItems}
-            onChange={onChange}
-            className='faq_collapse'
-            expandIcon={({ isActive }) => (
-              <Img rotate={isActive ? -180 : 0} size="large" />
-            )}
-          />
-        </div>
-        <div ref={whatAreWeDoingRef}>
-          <Header
-            title='Свойства онтологической безопасности'
-            description='<p>Возможность понимать, прогнозировать и удерживать на длинном горизонте планирования внутреннюю культуру и этику. К примеру, у бурят лес и духи леса священны. Зная эту онтологию, проектирование государственной институции в области охраны природы значительно упрощается— не нужно останавливать вырубки леса в Бурятии, потому что лес священен и его никто не тронет. Сохраняя культуру, айдентику и внутреннюю онтологию государство получает этическуюцелостность. На контуре внешнего мира, онтология это система, которая позволяет построить коммуникацию и язык с иным на взаимовыгодных условиях, без нарушения айдентики друг друга.</p>'
-          />
-          <p className='faq_info'>
-            Мы не нарушаем твою айдентику, ты ненарушаешь нашу — язык
-            коммуникации взаимовыгодный.
-          </p>
-          <div>
-            <Header
-              title='Онтологическая безопасность Армении'
-              description='<p>Возможность понимать, прогнозировать и удерживать на длинном горизонте планирования внутреннюю культуру и этику. К примеру, у бурят лес и духи леса священны. Зная эту онтологию, проектирование государственной институции в области охраны природы значительно упрощается— не нужно останавливать вырубки леса в Бурятии, потому что лес священен и его никто не тронет. Сохраняя культуру, айдентику и внутреннюю онтологию государство получает этическуюцелостность. На контуре внешнего мира, онтология это система, которая позволяет построить коммуникацию и язык с иным на взаимовыгодных условиях, без нарушения айдентики друг друга.</p>'
-            />
-            <Header title='Как это обеспечить — три примера' description='' />
-          </div>
-          <p className='faq_header'>Мы задаем себе вопросы:</p>
-          <Collapse
-            items={questionsItems}
-            onChange={onChange}
-            className='faq_collapse'
-            expandIcon={({ isActive }) => (
-              <Img rotate={isActive ? -180 : 0} size="large" />
-            )}
-          />
+        <div ref={whatAreWeDoingRef} className='faq_qesutions'>
+          {faq &&
+            //@ts-ignore
+            faq.map((f, i) => (
+              <div className='faq_question' key={i}>
+                <h1>{f["title_ru"]}</h1>
+                {f.id > 1 ? (
+                  <Collapse
+                    accordion
+                    bordered={false}
+                    className='faq_collapse'
+                    expandIcon={({ isActive }) => (
+                      <Img rotate={isActive ? -180 : 0} size='large' />
+                    )}>
+                    {
+                      //@ts-ignore
+                      f.question_answer.map((q, i) => (
+                        <Panel
+                          header={removeHtmlTags(q[`question_ru`])}
+                          key={i}
+                          className='faq_q'>
+                          <p className='faq_a'>
+                            {removeHtmlTags(q[`answer_ru`])}
+                          </p>
+                        </Panel>
+                      ))
+                    }
+                  </Collapse>
+                ) : (
+                  //@ts-ignore
+                  f.question_answer.map((q, i) => (
+                    <Fragment key={i}>
+                      <p className='faq_q'>
+                        {removeHtmlTags(q[`question_ru`])}
+                      </p>
+                      <span
+                        className='faq_a'
+                        dangerouslySetInnerHTML={{
+                          __html: q[`answer_ru`],
+                        }}
+                      />
+                    </Fragment>
+                  ))
+                )}
+              </div>
+            ))}
         </div>
         <div className='aboutUs_dashedLine' />
         <div className='inner aboutUs_privacy'>
